807c29c31646dacde7781603b31d48ef88f67a2d
Harindra Lingannagari <hlingannagari@salesforce.com>
2019-08-19 23:41:40 +0530

    Major commit today

--- js/custom/save_them_all.js
+++ js/custom/save_them_all.js
@@ -16,6 +16,39 @@ jQuery(document).ready(function() {
     for (let index = 37; index <= images; index++) {
         jQuery("#vunerable").append("<a href='images/mission/slider_1_"+(index)+".jpg' data-lightbox='mygallery'><img src='images/mission/slider_1_"+(index)+".jpg'></a>");
    };
+   var images = 48;
+   for (let index = 43; index <= images; index++) {
+     jQuery("#education").append("<a href='images/mission/slider_1_"+(index)+".jpg' data-lightbox='mygallery'><img src='images/mission/slider_1_"+(index)+".jpg'></a>");
+     };
+   var images = 12;
+   for (let index = 7; index <= images; index++) {
+     jQuery("#education").append("<a href='images/mission/slider_1_"+(index)+".jpg' data-lightbox='mygallery'><img src='images/mission/slider_1_"+(index)+".jpg'></a>");
+     }; 
+   var images = 18;
+   for (let index = 1; index <= images; index++) {
+     jQuery("#school_kits").append("<a href='images/kits/slider_1_"+(index)+".jpg' data-lightbox='mygallery'><img src='images/kits/slider_1_"+(index)+".jpg'></a>");
+     }; 
+   var images = 24;
+   for (let index = 1; index <= images; index++) {
+     jQuery("#sports_ent").append("<a href='images/sports/slider_1_"+(index)+".jpg' data-lightbox='mygallery'><img src='images/sports/slider_1_"+(index)+".jpg'></a>");
+     };
+   var images = 60;
+   for (let index = 37; index <= images; index++) {
+     jQuery("#culture_community").append("<a href='images/sports/slider_1_"+(index)+".jpg' data-lightbox='mygallery'><img src='images/sports/slider_1_"+(index)+".jpg'></a>");
+     };
+     var images = 36;
+   for (let index = 1; index <= images; index++) {
+     jQuery("#education_to_all").append("<a href='images/save/slider_1_"+(index)+".jpg' data-lightbox='mygallery'><img src='images/save/slider_1_"+(index)+".jpg'></a>");
+     };
+     var images = 36;
+   for (let index = 1; index <= images; index++) {
+     jQuery("#sports_entertainment").append("<a href='images/sports/slider_1_"+(index)+".jpg' data-lightbox='mygallery'><img src='images/sports/slider_1_"+(index)+".jpg'></a>");
+     };
+
+
+
+
+     
 });
 
 
